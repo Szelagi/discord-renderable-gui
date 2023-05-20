86c086ea5d39677418f73f5bc69d653f6a21373b
Szelagi <44713495+Szelagi@users.noreply.github.com>
2023-05-20 17:50:02 +0200

    Separate "gui" parallel class

---
+++ src/lib/ts/Gui.ts
@@ -0,0 +1,40 @@
+
+// Gui as object of class version
+// my note: initiator(params, data, builder)
+import Builder from "../Builder";
+
+class Gui {
+    #key : string;
+    #initiator;
+    #executor;
+    #watchers;
+
+    constructor(key) {
+        this.#key = key;
+        this.#watchers = [];
+    }
+
+    setInitiator(initiator) {
+        this.#initiator = initiator;
+    }
+
+    setExecutor(executor) {
+        this.#executor = executor;
+    }
+
+    addWatcher(...watcher) {
+
+    }
+
+    async create(initiatorParams) {
+        const dbg = {
+            id: undefined,
+            key: this.#key,
+            data: {}
+        }
+        const builder = new Builder();
+        await this.#initiator(initiatorParams, dbg.data, builder);
+        if (!builder.type) throw new Error('Unset builder option!');
+
+    }
+}
