4a518dfe23cada6305d6aa179c5d1f39360e5065
Szelagi <44713495+Szelagi@users.noreply.github.com>
2023-05-23 13:50:01 +0200

    Introduce create gui method

--- src/lib/ts/Gui.ts
+++ src/lib/ts/Gui.ts
@@ -2,18 +2,24 @@
 // Gui as object of class version
 // my note: initiator(params, data, builder)
 import Builder from "./Builder";
-import {Initiator} from "../types/interface";
+import {Initiator, StorageService} from "../types/interface";
 import {BuilderResult, SessionObject} from "../types/type";
+import {BuilderOptionEnum} from "../types/enum";
+import {ChatInputCommandInteraction, MessageCreateOptions, MessageInteraction, RepliableInteraction} from "discord.js";
+import {InteractionReplyOptions} from "discord.js";
+import SystemWatcher from "./SystemWatcher";
 
 export default class Gui<Input, Data> {
     #key : string;
+    #systemWatcher : SystemWatcher;
     #initiator : Initiator<Input, Data>;
     #executor;
     #watchers;
 
-    constructor(key: string) {
+    constructor(key: string, systemWatcher: SystemWatcher) {
         this.#key = key;
         this.#watchers = [];
+        this.#systemWatcher = systemWatcher;
     }
 
     setInitiator(initiator: Initiator<Input, Data>) {
@@ -30,16 +36,51 @@ export default class Gui<Input, Data> {
         return this;
     }
 
-    async create(initiatorParams) {
-        const dbg : SessionObject<Data> = {
-            id: null,
-            key: this.#key,
-            data: {} as Data
-        }
+    async create(initiatorParams: Input) {
+        const data : Data = {} as Data;
         const builder = new Builder();
-        const builderResult = await this.#initiator(initiatorParams, dbg.data, builder);
+        const builderResult = await this.#initiator(initiatorParams, data, builder);
+        let id: string;
         switch (builderResult.type) {
-
+            case BuilderOptionEnum.replyInteraction: {
+                // Compatible with MessageCreateOptions type
+                // https://old.discordjs.dev/#/docs/discord.js/main/typedef/InteractionReplyOptions
+                const responseTyped = builderResult.responseData as InteractionReplyOptions;
+                try {
+                    console.log(builderResult)
+                    responseTyped.fetchReply = true;
+                    const interaction = await builderResult.interaction.reply(responseTyped);
+                    const message = await interaction.fetch();
+                    id = message.id;
+                } catch (e) {
+                    console.error(e);
+                    return;
+                }
+                break;
+            }
+            case BuilderOptionEnum.replyMessage: {
+                const responseTyped = builderResult.responseData as MessageCreateOptions;
+                try {
+                    const message = await builderResult.message.reply(responseTyped);
+                    id = message.id;
+                } catch (e) {
+                    console.error(e);
+                    return;
+                }
+                break;
+            }
+            case BuilderOptionEnum.message: {
+                const responseTyped = builderResult.responseData as MessageCreateOptions;
+                try {
+                    const message = await builderResult.message.reply(responseTyped);
+                    id = message.id;
+                } catch (e) {
+                    console.error(e)
+                    return;
+                }
+                break;
+            }
         }
+        await this.#systemWatcher.storageService.insert(id, this.#key, data as object);
     }
 }
